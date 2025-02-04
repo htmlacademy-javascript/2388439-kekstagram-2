43e89167074ae6436b936f8b04c4e02bfdef6158
Vadim <vadimbox85@gmail.com>
2025-02-04 20:38:17 +0300

    Добавляет функцию проверки на палиндром

--- js/functions.js
+++ js/functions.js
@@ -6,3 +6,20 @@ function checkMaxLength(checkCharacterCount, maxLength){
 }
 
 checkMaxLength('Я люблю яблоки', 20);
+
+// Функция для проверки, является ли строка палиндромом.
+
+function palindromeCheck(stringCheck){
+  stringCheck = stringCheck.replaceAll(' ', '');
+  stringCheck = stringCheck.toLowerCase();
+  const len = stringCheck.length;
+
+  for(let i = 0; i < len / 2; i++){
+    if (stringCheck[i] !== stringCheck[len - 1 - i]){
+      return false;
+    }
+  }
+  return true;
+}
+
+palindromeCheck('Лёша на полке клопа нашёл');
