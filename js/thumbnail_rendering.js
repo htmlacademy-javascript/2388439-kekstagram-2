92b1ffea064b15482c89a8a712bfab5a3206c528
VadimPinyagin <vadimbox85@gmail.com>
2025-03-22 19:29:50 +0300

    исправление путей 574

--- js/thumbnail_rendering.js
+++ js/thumbnail_rendering.js
@@ -1,4 +1,4 @@
-import {album} from './upload';
+import {album} from './data.js';
 const template = document.querySelector('#picture').content.querySelector('.picture');
 const container = document.querySelector('.pictures');
 
