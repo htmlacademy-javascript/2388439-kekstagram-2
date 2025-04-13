7193e89f239c519deb1cfb292fa15da59c02f338
Keks <keks@htmlacademy.ru>
2025-04-13 18:58:24 +0300

    :heavy_check_mark: Сборка #9

--- 9/js/main.js
+++ 9/js/main.js
@@ -1,6 +1,6 @@
+import {openBigPicture} from './viewer_image.js';
 import './thumbnail_rendering.js';
 import {container} from './thumbnail_rendering.js';
-import {openBigPicture} from './image_viewer.js';
 
 container.addEventListener('click', (evt) => {
   const currentPictureNode = evt.target.closest('.picture');
