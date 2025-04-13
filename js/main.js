fcc866ff8a9c09a82afd1ba41788ba0844546667
Vadim Pinyagin <vadimbox85@gmail.com>
2025-04-13 12:08:46 +0300

    Исправление путей 2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import './thumbnail_rendering.js';
 import {container} from './thumbnail_rendering.js';
-import {openBigPicture} from './image_viewer.js';
+import {openBigPicture} from './viewer_image.js';
 
 container.addEventListener('click', (evt) => {
   const currentPictureNode = evt.target.closest('.picture');
