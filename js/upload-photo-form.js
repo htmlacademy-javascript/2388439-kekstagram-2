7ebbe04cfed7513405211536462e66133f182d55
Vadim <vadimbox85@gmail.com>
2025-04-29 15:46:26 +0300

    Добавляет простой валидатор для провреки пристина

--- js/upload-photo-form.js
+++ js/upload-photo-form.js
@@ -1,23 +1,13 @@
 import { isEscapeKey } from './utils.js';
 
-// 1.1. Загрузка нового изображения:
+const pageBody = document.querySelector('body');
+const uploadForm = document.querySelector('.img-upload__form'); //imgUploadForm
+const photoEditorForm = uploadForm.querySelector('.img-upload__overlay'); //uploadOverlay
+const uploadFileControl = uploadForm.querySelector('#upload-file'); //uploadFile
+const photoEditorResetBtn = photoEditorForm.querySelector('#upload-cancel'); //imaUploadCancel
 
-// выбор файла с изображением для загрузки;
-// изменение масштаба изображения;
-// применение одного из заранее заготовленных эффектов;
-// выбор глубины эффекта с помощью ползунка;
-// добавление текстового комментария;
-// добавление хэштегов.
-
-const uploadForm = document.querySelector('.img-upload__form'); // Форма для загрузки изображений на сайт
-const pageBody = document.querySelector('body'); // Тело документа.
-
-const uploadFileControl = uploadForm.querySelector('#upload-file'); //Поле для загрузки изображения.
-const photoEditorForm = uploadForm.querySelector('.img-upload__overlay'); // Форма редактирования изображения.
-const photoEditorResetBtn = photoEditorForm.querySelector('#upload-cancel'); // Кнопка для закрытия формы редактирования.
-
-const hashtagInput = uploadForm.querySelector('.text__hashtags'); //Поле для добавления хештега к изображению.
-const commentInput = uploadForm.querySelector('.text__description'); // Поле для добавления комментария к изображению.
+const commentInput = uploadForm.querySelector('.text__description');
+const hashtagInput = uploadForm.querySelector('.text__hashtags'); //inputHashtag
 
 const onPhotoEditorResetBtnClick = () => {
   closePhotoEditor();
@@ -55,8 +45,10 @@ export const initUploadModal = () => {
 const pristine = new Pristine(uploadForm, {
   classTo: 'img-upload__field-wrapper',
   errorClass: 'img-upload__field-wrapper--error',
-  errorTextParent: 'mg-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
 });
+
 pristine.addValidator(hashtagInput, (value) => {
-  console.log(/\d/.test(value));
-});
+  const hasNumber = /\d/.test(value);
+  return !hasNumber;
+}, 'Ошибонька');
