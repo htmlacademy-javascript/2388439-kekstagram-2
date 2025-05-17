5d723a1d5d255d81f2794484198e335051612adf
VadimPinyagin <vadimbox85@gmail.com>
2025-05-17 05:00:03 +0300

    Добавляет модуль Notificatin. js в котором находятся функции отработки сообщения после отправки/ошибки отправки формы на сервер

---
+++ js/notification.js
@@ -0,0 +1,20 @@
+import {isEscapeKey} from "./utils";
+
+const closeNotification = (evt) => {
+  evt.stopPropagation();
+  const existElement = document.querySelector('.success') || document.querySelector('.error');
+  const closeButton = existElement.querySelector('button');
+  if(evt.target === existElement || evt.target === closeButton || isEscapeKey(evt)) {
+    existElement.remove();
+    body.removeEventListener('click', closeNotification);
+    body.removeEventListener('keydown', closeNotification);
+  }
+};
+
+const appendNotification = (templeate, trigger = null) => {
+  trigger?.();
+  const notificationNode = templeate.cloneNode(true);
+  body.append(notificationNode);
+  body.addEventListener('click', closeNotification);
+  body.addEventListener('keydown', closeNotification);
+};
