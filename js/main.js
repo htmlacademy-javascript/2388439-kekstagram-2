4d16eee76665bbc395ebab654d867f07f838f158
VadimPinyagin <vadimbox85@gmail.com>
2025-05-21 19:14:44 +0300

    Добавляет функции от Кекса

--- js/main.js
+++ js/main.js
@@ -10,3 +10,25 @@ container.addEventListener('click', (evt) => {
 });
 
 initUploadModal();
+
+function debounce (callback, timeoutDelay = 500) {
+
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+function throttle (callback, delayBetweenFrames) {
+  let lastTime = 0;
+
+  return (...rest) => {
+    const now = new Date();
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+}
