5cbaf6091497825332650da1114c339653211730
VadimPinyagin <vadimbox85@gmail.com>
2025-05-22 17:44:26 +0300

    Добавляет функцию которая обрабатывает событие изменения фильтра

--- js/filter.js
+++ js/filter.js
@@ -8,3 +8,21 @@ const filterElement = document.querySelector('.img-filters');
 const ACTIVE_BUTTON_CLASS = 'img-filters__button--active';
 
 const debounceRender = debounce(openBigPicture);
+
+function onFilterChange(evt) {
+  const targetButton = evt.target;
+  const activeButton = document.querySelector(`.${ACTIVE_BUTTON_CLASS}`);
+  if(!targetButton.matches('button')) {
+    return;
+  }
+
+  if(activeButton === targetButton) {
+    return;
+  }
+  activeButton.classList.toggle(ACTIVE_BUTTON_CLASS);
+  targetButton.classList.toggle(ACTIVE_BUTTON_CLASS);
+  currentFilter = target.getAttribute('id');
+
+  applyfilter();
+}
+
